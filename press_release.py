8ead9526036e365bf686c8611050b64e4db44334
jay <jayujayanth421@gmail.com>
2023-08-03 22:27:44 +0530

    press_release.py added

---
+++ press_release.py
@@ -0,0 +1,17 @@
+press_release ="""
+
+MoneyWise is a basic banking system that allows users to create an account,
+deposit money, withdraw money, 
+and check their balance.
+It is built React, CSS and Supabase. 
+It has a simple and easy to use interface.
+It has 10 customers currently.
+It is hosted on Netlify.
+
+"""
+press_release.strip()
+
+press_release.replace("customers", "Happy Customers")
+
+
+print(press_release.strip().replace("customers", "Happy Customers").upper())
