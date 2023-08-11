18b1087e5d715eaa57eea5cb4d494cd6c060e025
jay <jayujayanth421@gmail.com>
2023-08-11 18:12:26 +0530

    nested_lists.PY added

---
+++ nested_lists.py
@@ -0,0 +1,23 @@
+couples = [["Beyonce", "Jay Z"], ["Kim K", "Kanye"], ["Chrissy", "John"]]
+#to print the first couple
+print(couples[0])
+#to print the second person in the first couple
+print(couples[0][1])
+#to print the second person in the last couple
+print(couples[-1][1])   
+#to print the first person in the last couple
+print(couples[-1][0])
+#to print the last person in the last couple
+print(couples[-1][-1])
+#to print the first person in the first couple
+print(couples[0][0])
+#to print the last person in the first couple
+print(couples[0][-1])
+#to print the last person in the second couple
+print(couples[1][-1])
+#to print the first person in the second couple
+print(couples[1][0])
+#to get first letter of the first person in the first couple
+couples[0][0][0]
+#to get first letter of the second person in the first couple
+couples[0][1][0]
