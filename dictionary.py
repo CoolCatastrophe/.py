06f09fb47b670545e354aed563843869b40a8133
jay <jayujayanth421@gmail.com>
2023-08-12 21:31:12 +0530

    dictionary.py, test.py modified

--- dictionary.py
+++ dictionary.py
@@ -1,1 +1,27 @@
 dict = {"name": "jayanth", "age": 21}
+
+# Accessing the values of a dictionary
+print(dict["name"])
+
+# Adding a new key-value pair to a dictionary
+dict["gender"] = "male"
+
+# Updating the value of a key in a dictionary
+dict["age"] = 22
+
+
+# Deleting a key-value pair from a dictionary
+popped = dict.pop("name")
+popitem = dict.popitem() # removes the last key-value pair
+print(dict)
+
+for key in dict:
+    print(key, dict[key])
+
+value = "jayanth" in dict.values()
+print(value)
+print(dict.values())
+
+print(dict.get("names"))
+
+
--- test.py
+++ test.py
@@ -1,3 +1,7 @@
 list = [1,2,3,4,5,6,7,8,9,10]
 
-print(range((list)))
+print(range(len(list)))
+
+list = [1, 2, 3, 4, 5]
+list.pop() # removes the last element
+print(list)
